b7dedaf6dbe6516e53a0c0d8101af7ccf87a170f
Jadynhong500 <jadynhong500@yahoo.com>
2019-08-24 10:57:39 -0500

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Em24';
-  page = 'home';
+export class AppComponent implements OnInit {
+    title = 'Em24';
+    page = 'home';
 
-  location: string;
-  type: string;
-  status: string;
-  equipment_needed: string;
-  severity: string;
-
-  public changePage(newPage: string) {
-    if(newPage === 'home') {
-      this.page = 'home';
-    }
-
-    if(newPage === 'report') {
-      this.page = 'report';
-    }
-
-    if(newPage === 'dispatch') {
-      this.page = 'dispatch';
-    }
-
-    if(newPage === 'workforce') {
-      this.page = 'workforce';
-    }
-
-  }
-
-    public makeNewReport() {
-
-  }
-
-}
-
-interface report {
-    id: string;
     location: string;
     type: string;
     status: string;
     equipment_needed: string;
     severity: string;
-}
 
-interface reportMap {
-    [key: string]: report;
+    incident_id: string;
+
+    ngOnInit(): void {
+    }
+
+    public changePage(newPage: string) {
+        if (newPage === 'home') {
+            this.page = 'home';
+        }
+
+        if (newPage === 'report') {
+            this.page = 'report';
+        }
+
+        if (newPage === 'dispatch') {
+            this.page = 'dispatch';
+        }
+
+        if (newPage === 'workforce') {
+            this.page = 'workforce';
+        }
+
+    }
+
+    public makeNewReport() {}
+
 }
